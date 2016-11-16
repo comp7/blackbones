0d3d768fba44c870126034f0eabb1cd3befcbf6c
mittelmabigkeit <comp422012016@mail.ru>
2016-11-16 15:34:48 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(function() {
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+    };
+
+    window.template = function(id) {
+        return _.template( $('#' + id). html() );
+    };
+
+    App.Models.Task = Backbone.Model.extend({
+        validate: function(attrs) {
+            if (! $.trim(attrs.title)) {
+                return 'Имя задачи должно быть валидным !';
+            }
+        }
+    });
+
+    App.Views.Task = Backbone.View.extend({
+        initialize: function() {
+            this.model.on('change', this.render, this);
+            this.model.on('destroy', this.remove, this);
+        },
+        tagName: 'li',
+        template: template('taskTemplate'),
+        render: function () {
+            var template = this.template(this.model.toJSON());
+            this.$el.html( template );
+            return this;
+        },
+        events:{
+            'click.edit': 'editTask',
+            'click.delete': 'destroy'
+        },
+        destroy: function () {
+            this.model.destroy();
+            console.log(tasksCollection);
+        },
+        remove: function(){
+            this.$el.remove();
+        },
+        editTask: function () {
+            var newTaskTitle = prompt('Как переименуем задачу ?', this.model.get('title'));
+            //if ( !newTaskTitle ) return;
+            this.model.set('title', newTaskTitle);
+        }
+    });
+
+    App.Collections.Task = Backbone.Collection.extend({
+        model: App.Models.Task
+    });
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+        render: function() {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+        addOne: function(task) {
+            var taskView = new App.Views.Task({ model:task });
+            this.$el.append(taskView.render().el);
+        }
+    })
+
+    window.tasksCollection = new App.Collections.Task([
+        {
+            title: 'Родиться',
+            priority: 4
+        },
+        {
+            title: 'Немножко пожить',
+            priority: 3
+        },
+        {
+            title: 'Умереть',
+            priority: 5
+        },
+
+    ]);
+
+    var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+
+    $('.tasks').html(tasksView.render().el);
+
+});
